c52a8ff6ae1f84ad00b84f848308b929de093562
Chin Chin Jim <chinchin.jim@mail.utoronto.ca>
2022-10-01 20:58:06 -0400

    Implemented Tasks 1-6 according to the instructions.

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -79,6 +79,13 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in two Strings as its arguments, and concatenates them.
+     * @param str1 a String for the first part of this Silly instance's name.
+     * @param str2 a String for the second part of this Silly instane's name.
+     */
+    public Silly(String str1, String str2){this.name = str1 + str2; }
 
 
 
@@ -116,7 +123,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +141,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -159,6 +167,7 @@ public class Silly implements Comparable<Silly>{
 
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
 
+        return this.name.equals(other.name);
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
     }
@@ -194,6 +203,17 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        int ret;
+        if(this.name.length() < other.name.length()){
+            ret = -1;
+        }
+        else if(this.name.length() == other.name.length()){
+            ret = 0;
+        }
+        else{
+            ret = 1;
+        }
+        return ret;
     }
 
     /*
